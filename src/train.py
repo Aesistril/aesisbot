00d2f88fb74bc14c216dc3e3b45f4053f09d9056
Yigit Ayaz <yigitayaz262@gmail.com>
2022-01-24 00:11:19 +0300

    turns out aesbot is already name of a discord bot

--- src/train.py
+++ src/train.py
@@ -1,16 +1,16 @@
-# Copyright 2022 Yiğit Ayaz
+# Copyright (C) 2022 Yiğit Ayaz
 
-# This file is part of Aesbot.
+# This file is part of Aesisbot.
 
-# Aesbot is free software: you can redistribute it and/or modify it under the terms 
+# Aesisbot is free software: you can redistribute it and/or modify it under the terms 
 # of the GNU General Public License as published by the Free Software Foundation, 
 # either version 3 of the License, or (at your option) any later version.
 
-# Aesbot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# Aesisbot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
 # PURPOSE. See the GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License along with Aesbot. 
+# You should have received a copy of the GNU General Public License along with Aesisbot. 
 # If not, see <https://www.gnu.org/licenses/>. 
 
 # TODO: Remove kind words like "please" (I think stemmer does this but I'm not sure)
